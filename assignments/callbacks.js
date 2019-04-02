aee7c75b1eb8792e3ea32edd9c9b943cfa77bcda
Your Name <you@example.com>
2019-04-02 14:45:34 -0500

    finished callbacks,assignments/callbacks.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,4 +1,6 @@
-// Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
+// Create a higher order function and invoke the callback function to test your work. You have been provided an example of 
+//a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out 
+//the rest of the problems.
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
@@ -28,24 +30,58 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
 }
+function getLength(arr,cb){
+  return cb(arr.length);
+}
+getLength(items,function(getLength){
+  console.log(getLength)
+});
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
 }
+function last(arr,cb){
+  return cb(arr[3]);
+}
+last(items, function(last) {
+  console.log(last)
+});
+
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
 }
+function sumNums(x,y,cb){
+return cb(x+y);
+}
+sumNums(3,4,function(sumNums){
+  console.log(sumNums);
+});
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
 }
+function multiplyNums(x,y,cb){
+  return cb(x*y);
+}
+multiplyNums(3,4,function(multiplyNums){
+  console.log(multiplyNums);
+});
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
 }
-
+function contains(item,list,cb){
+  if(list.includes(item)){
+    return cb(true)
+  } else {
+    return cb(false);
+  }
+}
+contains('Notebook',items,function(result){
+  console.log(result);
+})
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
